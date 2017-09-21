e1a24253cf4d31bbc8cc94ef42b94596cee056cf
ryanhamley <rhamley@skycatch.com>
2017-09-20 21:32:21 -0700

    Move config to component state

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,21 +1,48 @@
 import React, { Component } from 'react';
-import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
-import Config from '../../config/Config';
+import ReactMapboxGl, { Layer, Feature, ZoomControl } from 'react-mapbox-gl';
 
-const Map = ReactMapboxGl({
-  accessToken: Config.mapbox.token
-});
+let Map;
 
 class Mapbox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      config: {
+        token: 'pk.eyJ1IjoicmhhbWxleSIsImEiOiJjajd0cjZvN3MzbzlpMnFycnBtM3dmbWFyIn0.Toml4RrSL38x7vVqkm3JeQ',
+        style: {
+          "attributionControl": false,
+          "center": [-99.133209, 19.4326],
+          "keyboard": false,
+          "layers": [],
+          "maxZoom": 24,
+          "minZoom": 12,
+          "sources": {},
+          "style": "mapbox://styles/mapbox/streets-v10",
+          "version": 8,
+          "zoom": 16
+        }
+      }
+    };
+  }
+
+  componentWillMount() {
+    Map = ReactMapboxGl({
+      accessToken: this.state.config.token
+    });
+  }
+
   render() {
+    const config = this.state.config;
+
     return (
       <Map
-      style="mapbox://styles/mapbox/streets-v10"
-      center={Config.mapbox.style.center}
+      style={config.style.style}
+      center={config.style.center}
       containerStyle={{
         height: "100vh",
         width: "100vw"
       }}>
+        <ZoomControl/>
       </Map>
     );
   }
