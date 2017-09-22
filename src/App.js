73b674ac07016d05736b6ece437ff66cc1d7c87a
Sergio Emir Baidon Carrillo <sbaidon@outlook.com>
2017-09-22 10:24:41 -0400

    Change API to new endpoints, test GET Endpoints

--- src/App.js
+++ src/App.js
@@ -1,36 +1,14 @@
 import React, { Component } from 'react';
-import api from './api';
+// import api from './api';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.testApi();
 
     this.state = { date: new Date() };
   }
-  testApi() {
-    try {
-      api.getAcopios();
 
-      api.getProductos('1');
-
-      api.postAcopio({});
-      api.postProducto({});
-
-      api.postResponsable({});
-
-      api.updateAcopio('1', {});
-      api.updateProducto('1', {});
-      api.updateResponsable('1', {});
-
-      api.deleteAcopio('1');
-      api.deleteProducto('1');
-      api.deleteResponsable('1');
-    } catch (error) {
-      console.log(error);
-    }
-  }
   render() {
     return (
       <div className="App">
