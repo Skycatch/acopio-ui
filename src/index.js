7fbd60782568849fcf5430e8d579690d3ceafd9d
Luis del Río <luis@ioioio.mx>
2017-09-23 00:56:06 -0500

    remove router

--- src/index.js
+++ src/index.js
@@ -2,21 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Supply from './components/Supply'
 import registerServiceWorker from './registerServiceWorker';
 
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-
-
-ReactDOM.render((
-  <BrowserRouter>
-    <Switch>
-      <Route exact path='/' component={App}/>
-      <Route path='/supply' component={Supply}/>
-    </Switch>
-  </BrowserRouter>
-), document.getElementById('root'))
-
-
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
-
