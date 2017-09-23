2b98b44373b15cb2783a4eb6a9a2a722c0fd2b2f
Luis del Río <luis@ioioio.mx>
2017-09-23 08:48:49 -0500

    linting

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,7 +3,6 @@ import logoImage from './CMX_SISMO_ICON_04-01.png'
 
 const Layout = ({ children }) => {
   return (
-
     <div className="App drawer-container">
       <div className="App-header">
         <div className="logo">
@@ -11,17 +10,14 @@ const Layout = ({ children }) => {
           <h1 className="title"><a href="/">Sismo MX</a></h1>
           <h2 className="smalltitle">| Centros de acopio</h2>
         </div>
-        
+
         <h1 className="sub-title">Información de centros de acopio</h1>
         <a href="/map">Mapa</a>
       </div>
 
-
       <div className="App-body">
         {children}
       </div>   
-
-
     </div>
   )
 }
