243f29779a6b9f02356ba35f72ad3e6a208f4c60
Rodrigo Quezada <roderik.ikana@gmail.com>
2017-09-23 16:38:43 -0500

    fix: pr changes

--- src/screens/CenterAdmin.js
+++ src/screens/CenterAdmin.js
@@ -10,39 +10,46 @@ import {
   RaisedButton,
 } from 'material-ui'
 
+import { browserHistory } from 'react-router-dom'
+
 import './CenterAdmin.css'
 
 import api from '../api'
-  
 
-class CenterAdmin extends Component{
-    constructor (props) {
-        super(props)
-        this.state = {
-            loading: true,
-            productList: [],
-            filteredProdcuts: [],
-            newProduct: '',
-            filter: '',
-            selected: []
-        }
+const serial = fn =>
+  fn.reduce((promise, fn) =>
+    promise.then(result => fn().then(Array.prototype.concat.bind(result))),
+  Promise.resolve([]))
+
+class CenterAdmin extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      loading: true,
+      productList: [],
+      filteredProducts: [],
+      newProduct: '',
+      filter: '',
+      selected: []
     }
+  }
 
-    componentDidMount() {
-        const { id } = this.props.match.params
-        const product = api.getProductosByAcopioId(id).then(({data: productList}) => {
-          this.setState(() => ({ productList, filteredProdcuts: productList }))
-        })
+  componentDidMount () {
+    console.log({ browserHistory })
+    const { id } = this.props.match.params
+    const product = api.getProductosByAcopioId(id).then(({data: productList}) => {
+      this.setState(() => ({ productList, filteredProducts: productList }))
+    })
 
-        product.then(() => {
-            this.setState(() => ({ loading: false }))
-        })
-    }
-    onChangeFilte = (evt, filter) => {
+    product.then(() => {
+      this.setState(() => ({ loading: false }))
+    })
+  }
+    onChangeFilter = (evt, filter) => {
       const { productList } = this.state
       const inputValue = filter.trim().toLowerCase()
       const inputLength = inputValue.length
-      const filteredProducts =  inputLength === 0 ? productList : productList.filter(({nombre}) =>
+      const filteredProducts = inputLength === 0 ? productList : productList.filter(({nombre}) =>
         nombre.toLowerCase().slice(0, inputLength) === filter
       )
       this.setState(() => ({ filteredProducts, filter }))
@@ -65,56 +72,51 @@ class CenterAdmin extends Component{
       })
     }
     onDelete = () => {
-      const { selected, filteredProdcuts } = this.state
+      const { selected, filteredProducts } = this.state
       if (selected === 'none') return
       this.setState(() => ({ loading: true }))
-      const toDelete = (selected === 'all' ? filteredProdcuts : selected.map(index => filteredProdcuts[index])).map(({ id }) => id)
+      const toDelete = (selected === 'all' ? filteredProducts : selected.map(index => filteredProducts[index])).map(({ id }) => id)
 
-      const serial = fn =>
-        fn.reduce((promise, fn) =>
-          promise.then(result => fn().then(Array.prototype.concat.bind(result))),
-          Promise.resolve([]))
       const fns = toDelete.map(id => () => api.deleteProducto(id))
       serial(fns).then((res) => {
         window.location.reload()
       })
     }
     render () {
-        const { newProduct, filteredProdcuts, filter, selected } = this.state;
-        return this.state.loading ? <h1>Loading</h1> : (<div className="CenterAdmin">
-          <TextField
-            hintText="Nuevo Producto"
-            value={newProduct}
-            onChange={(evt, newProduct) => { this.setState(() => ({ newProduct })) }}
-          />
-          <br/>
-          <RaisedButton label="Crear" primary onClick={this.onSave} />
-          <br/>
-          <TextField
-            hintText="Filtrar"
-            value={filter}
-            onChange={this.onChangeFilte}
-          />
-          <br/>
-          <RaisedButton label="Borrar" secondary onClick={this.onDelete} />
-          <Table multiSelectable onRowSelection={this.onRowSelection} >
-            <TableHeader>
-              <TableRow>
-                <TableHeaderColumn>Nombre</TableHeaderColumn>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {filteredProdcuts.map(({ nombre, id }, index) => 
-                (<TableRow key={id} selected={selected.indexOf(index) !== -1}>
-                  <TableRowColumn>{nombre}</TableRowColumn>
-                </TableRow>)
-              )}
-            </TableBody>
-          </Table>
-          <RaisedButton label="Borrar" secondary onClick={this.onDelete} />
-        </div>)
+      const { newProduct, filteredProducts, filter, selected } = this.state
+      return this.state.loading ? <h1>Loading</h1> : (<div className="CenterAdmin">
+        <TextField
+          hintText="Nuevo Producto"
+          value={newProduct}
+          onChange={(evt, newProduct) => { this.setState(() => ({ newProduct })) }}
+        />
+        <br />
+        <RaisedButton label="Crear" primary onClick={this.onSave} />
+        <br />
+        <TextField
+          hintText="Filtrar"
+          value={filter}
+          onChange={this.onChangeFilter}
+        />
+        <br />
+        <RaisedButton label="Borrar" secondary onClick={this.onDelete} />
+        <Table multiSelectable onRowSelection={this.onRowSelection} >
+          <TableHeader>
+            <TableRow>
+              <TableHeaderColumn>Nombre</TableHeaderColumn>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {filteredProducts.map(({ nombre, id }, index) =>
+              (<TableRow key={id} selected={selected.indexOf(index) !== -1}>
+                <TableRowColumn>{nombre}</TableRowColumn>
+              </TableRow>)
+            )}
+          </TableBody>
+        </Table>
+        <RaisedButton label="Borrar" secondary onClick={this.onDelete} />
+      </div>)
     }
-
 }
 
 export default CenterAdmin
